1fc91f3c3b8c0f33374fd244bdc7275512c549e5
Glitch (crawling-stupendous-limburger) <none>
2023-02-22 15:22:52 +0000

    🏗🎰 Checkpoint ./commands/yardım.js:36838080/323 ./index.js:36838080/1300

--- commands/yardım.js
+++ commands/yardım.js
@@ -14,20 +14,19 @@ module.exports = {
     .setFooter({ text: `Anka Code • Alfa#2522`, iconURL: 'https://media.discordapp.net/attachments/1067780547159539762/1077969319654334545/static.png' })
     .setImage("https://media.discordapp.net/attachments/1064266014671503380/1075885849557880952/Screenshot_1.png?width=623&height=125")
     .setDescription(`
-> **🎈refix:** */*
-> <:r_tac:1076950185231532183> Sahib: <@871711574593789962>
-> <:r_yesil:1076950445530026035> Ping: \`15\`
+> **Prefix:** */*
+> Sahip: <@871711574593789962>
+> Ping: \`15\`
 
 *Ana Komutlar;*
 
-<:r_ayarlar:1077328118047592579> \`Moderasyon\` **hakkında bilgi alabilirsiniz.**
-<:r_amungus:1076950579512881172> \`Kayıt\` **hakkında bilgi alabilirsiniz**
-<:r_kullanici:1077587198989316117> \`Kullanıcı\` **hakkında bilgi alabilirsiniz.**
+\`Moderasyon\` **hakkında bilgi alabilirsiniz.**
+\`Kayıt\` **hakkında bilgi alabilirsiniz**
+\`Kullanıcı\` **hakkında bilgi alabilirsiniz.**
 
 *Bağlantılar;*
-<:r_discord:1077587395001729034> [Destek Sunucusu](https://discord.gg/vmj7wqZF5A)
-<:r_bot:1077587502157807616> [Botu Ekle](https://discord.gg/vmj7wqZF5A)
-<:r_tik:1077587538128154635> [Oyver](https://discord.gg/vmj7wqZF5A`)
+[Destek Sunucusu](https://discord.gg/vmj7wqZF5A)
+[Botu Ekle](https://discord.gg/vmj7wqZF5A)`)
     .setColor("#000000")
     const row = new Discord.ActionRowBuilder()
     .addComponents(
