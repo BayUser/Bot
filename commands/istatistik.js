7ab6148188cc37de27575613f72b742f635a68a2
Glitch (crawling-stupendous-limburger) <none>
2023-05-26 07:48:17 +0000

    🌨🏞 Checkpoint ./commands/hg-bb.js:53450312/2 ./commands/istatistik.js:53450312/182

--- commands/istatistik.js
+++ commands/istatistik.js
@@ -15,7 +15,7 @@ module.exports = {
     const embed = new EmbedBuilder()
     .setColor('#323338')
     .setTitle("• Lunar | İstatistik")
-    .setDescription("__**Genel Veriler**__\n• Toplam Kullanıcı Sayısı: \n•Toplam Sunucu Sayısı: \n•Toplam Komut Sayısı: \n\n__**Bot Bilgileri**__\n•Ping Değeri: ${client.ws.ping}")
+    .setDescription(`__**Genel Veriler**__\n• Toplam Kullanıcı Sayısı: **${userCount}**\n• Toplam Sunucu Sayısı: **${client.guilds.cache.size}**\n• Toplam Komut Sayısı: **30**\n\n__**Bot Bilgileri**__\n• Ping Değeri: **${client.ws.ping}**`)
     .setImage("https://media.discordapp.net/attachments/1071467515252838471/1110965641004978207/20230524_184614.jpg")
     interaction.reply({embeds: [embed]})
   }
