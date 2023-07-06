a229a704aa278a9d6656af70402c67637384a6b3
Glitch (giddy-dirt-lavender) <none>
2023-07-06 13:09:01 +0000

    🍍🍐 Checkpoint

--- commands/test.js
+++ commands/test.js
@@ -5,62 +5,26 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const db = require("croxydb")
 
  module.exports = {
-
-  name: "test",
-
-  description: "Test komutu.",
-
+  name: "premium",
+  description: "Premium komutu.",
   type: 1,
-
   options: [],
 
     // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
 
     run: async (client, interaction) => {
 
-      const Duyuru = db.fetch(`Duyurular`)
+      const pre = db.fetch(`Premiums`)
 
-      if(!Duyuru) {
+      if(!pre) {
 
        
 
       const Yardım = new EmbedBuilder()
 
          .setColor("Blurple")
-
-         .setImage("https://media.discordapp.net/attachments/1004368050038001804/1086953067301310524/standard_1.gif")
-
-         .setTitle("Fr3zy Bot • Yardım menüsü")
-
-         .setDescription(`
-
-</yardım:0> • Yardım menüsünü gösterir.
-
-> **Uptime Komutları**
-
-</link-say:0> • Sistemdeki link sayılarını gösterir.
-
-</link-ekle:0> • Sisteme link eklersiniz.
-
-</link-sil:0> • Sistemden link silersiniz.
-
-</link-liste:0> • Sistemdeki linklerinizi listeler.
-
-> **Bot Komutları**
-
-</istatistik:0> • Bot istatistiklerini gösterir.
-
-         
-
-</ping:0> • Botun ping değerlerini gösterir.
-
-         
-
-**<:zil:1121840973870288956> Bot duyuruları**
-
-Aktif bir duyuru bulunmuyor.
-
-`)
+         .setFooter({name: "Premium • oguzh3n"})
+         .setDescription(`> Premium satın almak için [destek sunucumuza]() gelebilirsin`)
 
       interaction.reply({embeds: [Yardım]})
 
