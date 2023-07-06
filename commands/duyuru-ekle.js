f5c32f58f073b7891dc17550f79f4d7cb34f0a24
Glitch (giddy-dirt-lavender) <none>
2023-07-06 09:30:39 +0000

    🏰🌱 Checkpoint ./commands/duyuru-ekle.js:53450312/695

--- commands/duyuru-ekle.js
+++ commands/duyuru-ekle.js
@@ -3,23 +3,26 @@ const db = require("croxydb");
 
 module.exports = {
   
-    data: new SlashCommandBuilder()        .setName('duyuru-ekle')
-      .setDescription('🌟 Sisteme bir duyuru ekler.')
-      .setDMPermission(false)
-      .addStringOption(option =>
-        option
-          .setName('duyuru')
-          .setDescription('Eklenecek duyuruyu belirtin.')
-          .setRequired(true)),
+      name: ('duyuru-ekle'),
+      description: ('Duyuru komutu.'),
+      type: 1,
+      options: [
+        {
+          name:"duyuru",
+          description:"Duyuru seçeneği.",
+          type:3,
+          required:true
+        }
+      ],
 
           run: async (client, interaction) => {      
 
       
 
-      const YetkiYok = new EmbedBuilder()
-        .setDescription(`**<:carpi:1121840969340420127> Bu komutu kullanabilmek için \`Bot sahibi\` olmalısın.**`)
+      const YetkiYok = new Client.EmbedBuilder()
+        .setDescription(`• Bu komutu kullanabilmek için \`developer\` olmalısın.`)
         .setColor('Red')
-        .setTitle("Olamaz yetkin yok")
+        .setTitle("Hata")
 
       if(interaction.user.id !== "963347002052214824"){
       return interaction.reply({embeds: [YetkiYok]})
