76c8c4e2186dc1fadf1d6a28ea61bfad8404d153
Glitch (crawling-stupendous-limburger) <none>
2023-07-06 08:49:40 +0000

    🍛🕹 Checkpoint ./commands/yardım.js:53450312/1132

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,5 +1,7 @@
 const { Client, EmbedBuilder } = require("discord.js");
 const Discord = require("discord.js")
+const db = require("croxydb")
+
 module.exports = {
   name: "yardım",
   description: "Yardım komutu.",
@@ -7,17 +9,24 @@ module.exports = {
   options: [],
 
   run: async(client, interaction) => {
-//ANKA CODE
+    
+    const Duyuru = db.fetch(`Duyurular`)
+
+      if(!Duyuru) {
+        
     const embed = new Discord.EmbedBuilder()
     .setAuthor({ name: `Mechatron | Yardım Menüsü` })
     .setFooter({ text: `OguZ • ȣ OguZ#2600` })
     .setImage("https://media.discordapp.net/attachments/1125194165513375804/1125437444565782548/20230703_174627.jpg")
-    .setDescription(`> Herhangi bir yardıma ihtiyacın olursa seni [destek sunucumuzda](https://discord.gg/MWBMqd7jjz) bekliyor olacağız.\n\n__**Kategoriler**__\n*Moderasyon*\n*Kullanıcı*\n*Kayıt*\n\n__**Linkler**__\n[Botu Davet Et](https://discord.com/oauth2/authorize?client_id=1090346236655173712&scope=bot&permissions=8)\n[Destek Sunucusu](https://discord.gg/MWBMqd7jjz)`)
+    .setDescription(`> Herhangi bir yardıma ihtiyacın olursa seni [destek sunucumuzda](https://discord.gg/MWBMqd7jjz) bekliyor olacağız.\n\n__**Duyurular**__\nAktif bir duyuru bulunmuyor.\n\n__**Kategoriler**__\n*Moderasyon*\n*Kullanıcı*\n*Kayıt*\n\n__**Linkler**__\n[Botu Davet Et](https://discord.com/oauth2/authorize?client_id=1090346236655173712&scope=bot&permissions=8)\n[Destek Sunucusu](https://discord.gg/MWBMqd7jjz)`)
     .setColor("#323338")
     
     interaction.reply({embeds: [embed], ephemeral: true})
-  }
-
+  } else {
+        const duyurular = db.fetch(`Duyurular`).map(y => `> \`${y}\``).join("\n")
+        
+ }
+    }
 };
 
 // Designed And Coded By OguZ INC.
