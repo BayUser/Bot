e0b430f217018fda8893dc0f74e9c552810fdf2b
Glitch (giddy-dirt-lavender) <none>
2023-07-10 13:31:51 +0000

    📠🙅 Checkpoint ./commands/istatistik.js:53450312/4

--- commands/istatistik.js
+++ commands/istatistik.js
@@ -16,7 +16,7 @@ module.exports = {
     const embed = new EmbedBuilder()
     .setColor('#323338')
     .setTitle("• Mechatron | İstatistik")
-    .setDescription(`<:emoji_17:1126458770973806673> **|** Genel Veriler\n• Toplam Kullanıcı Sayısı: **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n• Toplam Sunucu Sayısı: **${client.guilds.cache.size}**\n• Toplam Komut Sayısı: **30**\n\n<:emoji_14:1126458666393014322> **|** Bot Bilgileri\n• Ping Değeri: **${client.ws.ping}**\n• Uptime: **${Uptime}**`)
+    .setDescription(`<:emoji_17:1126458770973806673> **|** Genel Veriler\n• Toplam Kullanıcı Sayısı: **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n• Toplam Sunucu Sayısı: **${client.guilds.cache.size}**\n• Toplam Komut Sayısı: **40**\n\n<:emoji_14:1126458666393014322> **|** Bot Bilgileri\n• Ping Değeri: **${client.ws.ping}**\n• Uptime: **${Uptime}**`)
     .setImage("https://media.discordapp.net/attachments/1125194165513375804/1125437444565782548/20230703_174627.jpg")  .setImage("https://media.discordapp.net/attachments/1125194165513375804/1125377339707113552/20230703_134738.jpg")
     interaction.reply({embeds: [embed]})
   }
