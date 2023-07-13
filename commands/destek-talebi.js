d15082ff94a6ab79a86780faa73b5eba84e81493
Glitch (giddy-dirt-lavender) <none>
2023-07-13 22:02:18 +0000

    🖇🐺 Checkpoint ./commands/destek-talebi.js:53450312/124

--- commands/destek-talebi.js
+++ commands/destek-talebi.js
@@ -1,35 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-        .setName('ticket'),
-        .setDescription('Destek talebi oluşturun'),
+  name:('ticket'),
+  description:('Destek talebi oluşturun'),
+  type:1,
+  options:[],
 
-    run: async execute(interaction) {
+    run: async (interaction) => {
 
         // Destek talebi oluşturma
-
         const channel = await interaction.guild.channels.create(`ticket-${interaction.user.id}`);
-
-        const supportRole = interaction.guild.roles.cache.find(role => role.name === 'Destek Ekibi'); // Destek ekibi rolü adını buraya yazın
-
+        const supportRole = interaction.guild.roles.cache.find(role => role.name === 'Staff'); // Destek ekibi rolü adını buraya yazın
         const everyoneRole = interaction.guild.roles.everyone;
 
         channel.permissionOverwrites.create(supportRole, { VIEW_CHANNEL: true, SEND_MESSAGES: true });
-
         channel.permissionOverwrites.create(everyoneRole, { VIEW_CHANNEL: false });
 
         const embed = new MessageEmbed()
-
             .setTitle('Destek Talebi')
-
             .setDescription('Destek talebiniz oluşturuldu, bir yetkili en kısa sürede sizinle ilgilenecektir.')
-
-            .setColor('#FF0000');
+            .setColor('#323338');
 
         channel.send({ content: `${interaction.user}`, embeds: [embed] });
-
         interaction.reply({ content: 'Destek talebiniz oluşturuldu.', ephemeral: true });
 
     },
