9a249059bbf12c58bf0879efc296ca96a96cdefd
Glitch (giddy-dirt-lavender) <none>
2023-07-14 12:53:35 +0000

    🍯🎿 Checkpoint ./commands/ticket.js:53450312/1933

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,35 +1,31 @@
-const { Client, EmbedBuilder, MessageButton, MessageActionRow, ButtonBuilder } = require('discord.js');
+const { MessageActionRow, MessageButton, EmbedBuilder, ButtonBuilder, MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'ticket',
-    description: 'Ticket komutu.',
-    type:1,
-    options:[],
+  name: 'ticket',
+  description: 'Ticket komutu.',
+  type: 1,
+  options: [],
 
-    run: async (interaction) => {
-    const { user, guildId, channel } = interaction;
+  run: async (interaction) => {
+    const { user, guildId, channelId } = interaction;
 
     // Buton oluşturma
-
     const button = new ButtonBuilder()
       .setCustomId('create_ticket')
       .setLabel('Ticket Oluştur')
-      .setStyle('Primary'); 
+      .setStyle('PRIMARY');
 
     // Butonu içeren Action Row'u oluşturma
-      const row = new MessageActionRow()
-      .addComponents([button]);
+    const row = new MessageActionRow()
+      .addComponents(button);
 
     // Embed oluşturma
-
     const ticketEmbed = new EmbedBuilder()
       .setTitle('Ticket Oluşturma')
       .setDescription('Ticket oluşturmak için aşağıdaki butona tıklayın.')
-      .setColor('#323338');   
+      .setColor('#323338');
 
     // Komutun yanıtı olarak embed ve butonlu mesajı gönderme
-
-    await interaction.reply({embeds: [ticketEmbed],components: [row],ephemeral: true,});
+    await interaction.reply({ embeds: [ticketEmbed], components: [row], ephemeral: true });
   },
 };
-
