f8cc0d8f430d5e8a6bd6e131cb8a9528530cc13b
Glitch (giddy-dirt-lavender) <none>
2023-07-16 19:36:47 +0000

    🎷🚧 Checkpoint ./commands/herkeserolver.js:53450312/1174

--- commands/herkeserolver.js
+++ commands/herkeserolver.js
@@ -1,5 +1,5 @@
-const { EmbedBuilder, MessageButton, MessageActionRow } = require("discord.js");
-const Discord = require("discord.js")
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
   name: 'herkese-rol-ver',
@@ -14,62 +14,54 @@ module.exports = {
   }
   ],
 
- run:async (client, args, interaction) => {
+    run: async (client, args, interaction) => {
 
     if (!interaction.member.permissions.has("MANAGE_ROLES"))
     return interaction.reply("• Bu komut için yeterli yetkiye sahip değilsin.")
      
-    let rol =
-    interaction.mentions.roles.first() ||
-    interaction.guild.roles.cache.get(args[0]) ||
-    interaction.guild.roles.cache.find(rol => rol.name === args[0]);
-    
-    const row = new InteractionRow()
+    let rol = interaction.options.getMember('rol')
+        
+    const row = new ActionRowBuilder()
     .addComponents(
-     new MessageButton()
+     new ButtonBuilder()
      .setLabel("Evet")
      .setStyle("SUCCESS")
      .setCustomId("evet"),
-     new MessageButton()
+     new ButtonBuilder()
      .setLabel("Hayır")
      .setStyle("DANGER")
      .setCustomId("h"))
 
-const a = new MessageEmbed()
+const a = new EmbedBuilder()
 .setTitle("Godzilla - Herkese Rol Verme Sistemi!")
 .setDescription(`Sunucudaki Bütün Üyelere ${rol} Rolünü Vermek İstediğine Emin Misin?`)
 .setColor("GOLD")
-message.channel.send({embeds: [a], components: [row]}).then(radio => {
+interaction.channel.send({embeds: [a], components: [row]}).then(radio => {
     radio.createMessageComponentCollector(user => user.clicker.user.id == interaction.author.id).on('collect', async (button) => {
       let interaction = button
         if (interaction.customId == "h") {
-          const embedd = new Discord.MessageEmbed()
-          .setDescription(`Bu butonu sadece komutu yazan (<@${message.author.id}>) kullanabilir.`)
-          .setColor("RED")
-          if(interaction.user.id !== message.author.id) return interaction.reply({embeds: [embedd], ephemeral: true})
-          message.channel.send("İşlem İptal Edildi!")  
+
+          if(interaction.user.id !== interaction.user.id) return interaction.reply({content:"• Birşeyler ters gitti.", ephemeral: true})
+          interaction.channel.send("• İşlem iptal edildi.")  
         radio.delete()
         }
         if (interaction.customId == "evet") {
-          const embedd = new Discord.MessageEmbed()
-          .setDescription(`Bu butonu sadece komutu yazan (<@${message.author.id}>) kullanabilir.`)
-          .setColor("RED")
-          if(interaction.user.id !== message.author.id) return interaction.reply({embeds: [embedd], ephemeral: true})
-    const embed313 = new MessageEmbed()
+          if(interaction.user.id !== interaction.user.id) return interaction.reply({content:"• Birşeyler ters gitti.", ephemeral: true})
+    const embed313 = new EmbedBuilder()
     .setTitle("Godzilla - Rol Verme Sistemi!")
     .setDescription("Herkese Rolü Dağıtıyorum...")
     .setColor("GOLD")
   radio.edit({embeds: [embed313], components: []}).then(msg => {
-  message.guild.members.cache.forEach(u => {
+  interaction.guild.members.cache.forEach(u => {
     u.roles.add(rol);
     setTimeout(() => {
     }, 1000)
   });
-  const embed = new MessageEmbed()
+  const embed = new EmbedBuilder()
     .setDescription(`✅ **Herkese ${rol} Adlı Rol Verildi!**`)
-
     .setTitle("Godzilla - Rol Verme Sistemi!")
     .setColor("GOLD")
+  
  radio.edit({embeds: [embed]});
   
  
