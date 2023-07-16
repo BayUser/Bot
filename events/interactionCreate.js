8272288e0261ff3b9a33bafe51457f42bdf9d015
Glitch (giddy-dirt-lavender) <none>
2023-07-16 07:59:48 +0000

    ⛈👶 Checkpoint ./events/interactionCreate.js:53450312/8913

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,30 +1,43 @@
-const { Collection, Client } = require("discord.js");
+const { Collection, EmbedBuilder, Client, Interaction, MessageButton, MessageActionRow } = require("discord.js");
 const db = require("croxydb");
+const Discord = require("discord.js");
 const { readdirSync } = require("fs");
+const { colors, fromIntToDate } = require('discord-toolbox');
 const config = require('../config.json');
+const moment = require('moment');
 
-module.exports = async (client, interaction) => {
-  if (interaction.isCommand()) {
-    if (!interaction.guildId) return;
+module.exports = async(client, interaction) => {
+  
+  if(interaction.isChatInputCommand()) {
+  if (!interaction.guildId) return;
 
-    readdirSync('./commands').forEach(file => {
-      const command = require(`../commands/${file}`);
+    readdirSync('./commands').forEach(f => {
 
-      if (interaction.commandName.toLowerCase() === command.name.toLowerCase()) {
-        return command.run(client, interaction, db);
-      }
-    });
-  }
+      const cmd = require(`../commands/${f}`);
 
-  const butonrol = db.fetch(`buton_rol${interaction.guild.id}`);
+      if(interaction.commandName.toLowerCase() === cmd.name.toLowerCase()) {
 
-  if (interaction.isButton()) {
-    if (!interaction.member.roles.cache.has(butonrol)) {
-      await interaction.member.roles.add(butonrol);
-      interaction.reply({ content: "<:tik:1039607067729727519> | Rol Başarıyla Verildi!", ephemeral: true });
-    } else {
-      await interaction.member.roles.remove(butonrol);
-      interaction.reply({ content: "<:carpi:1040649840394260510> | Rol Başarıyla Alındı!", ephemeral: true });
+        return cmd.run(client, interaction, db);
+
     }
-  }
-};
+    });
+    }
+
+  
+
+    const butonrol = db.fetch(`buton_rol${interaction.guild.id}`)
+
+    if(interaction.customId === "rol") {
+    if(!interaction.member.roles.cache.has(butonrol)) {    
+
+      interaction.member.roles.add(butonrol)
+      interaction.reply({content: "<:tik:1039607067729727519> | Rol Başarıyla Verildi!", ephemeral: true})
+
+    } else {      
+
+      interaction.member.roles.remove(butonrol)
+      interaction.reply({content: "<:carpi:1040649840394260510> | Rol Başarıyla Alındı!", ephemeral: true})
+
+    }
+    }  
+    };
